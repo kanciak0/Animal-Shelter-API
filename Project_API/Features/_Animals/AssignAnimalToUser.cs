2020e08e282faf51e6c9d24dd54fad298204e86d
Artur Madejski <artur.madejski.kanciak@wp.pl>
2023-04-06 19:03:15 +0200

    feat:Added AssignAnimalToUser Request

--- Project_API/Features/_Animals/AssignAnimalToUser.cs
+++ Project_API/Features/_Animals/AssignAnimalToUser.cs
@@ -1,6 +1,67 @@
-﻿namespace Project_API.Features._Animals
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Project_API.Common;
+using Project_API.Features.User;
+using Project_API.Infrastructure.Persistence;
+
+namespace Project_API.Features._Animals
 {
-    public class AssignAnimalToUser
+    public class AssignAnimatoUserController : ApiControllerBase
     {
+        [HttpPut("{uuid}")]
+        public async Task<IActionResult> Assign([FromRoute] Guid uuid, [FromBody] AssignAnimalToUserCommand command)
+        {
+            command.Uuid = uuid;
+            await Mediator.Send(command);
+
+            return NoContent();
+        }
+
+    }
+    public class AssignAnimalToUserCommand : IRequest<Unit>
+    {
+        public Guid Uuid { get; set; }  
+        public ICollection<Guid> AnimalUuids { get; set; } = new List<Guid>();
+    }
+
+        internal class AssignAnimalToUserCommandHandler : IRequestHandler<AssignAnimalToUserCommand, Unit>
+    {
+        private readonly DemoDatabaseContext _dbcontext;
+
+        public AssignAnimalToUserCommandHandler(DemoDatabaseContext dbContext)
+        {
+            _dbcontext = dbContext;
+        }
+        public async Task<Unit> Handle(AssignAnimalToUserCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var user = await _dbcontext.Users.Include(u => u.Animals).FirstOrDefaultAsync(x => x.UUID == request.Uuid, cancellationToken);
+                if (user == null)
+                {
+                    throw new Exception("User not found");
+                }
+                foreach (var animalUuid in request.AnimalUuids)
+                {
+                    if (!user.Animals.Any(a => a.Animal_UUID == animalUuid))
+                    {
+                        var animal = await _dbcontext.Animals.FirstOrDefaultAsync
+                            (x => x.Animal_UUID == animalUuid, cancellationToken);
+                        if (animal == null)
+                        {
+                            throw new Exception($"Animal with UUID {animalUuid} not found"); //TODO: Make validation for animals through db like it is below instead
+                        }
+                        user.Animals.Add(animal);
+                    }
+                }
+                await _dbcontext.SaveChangesAsync(cancellationToken);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            return Unit.Value;
+        }
     }
 }
--- Project_API/Features/_Animals/DeleteAnimal.cs
+++ Project_API/Features/_Animals/DeleteAnimal.cs
@@ -11,7 +11,7 @@ namespace Project_API.Features.User
     {
 
         [HttpDelete("{id}")]
-        public async Task<ActionResult> Delete(Guid id)
+        public async Task<ActionResult> Delete([FromRoute]Guid id)
         {
             await Mediator.Send(new DeleteAnimalCommand { Id = id });
 
