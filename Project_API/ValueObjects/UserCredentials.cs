434783ddb94996ad9c2ec27eb464798fc1f992d4
Artur Madejski <artur.madejski.kanciak@wp.pl>
2023-04-08 21:23:19 +0200

    feat: UserCredentials Value object

--- Project_API/ValueObjects/UserCredentials.cs
+++ Project_API/ValueObjects/UserCredentials.cs
@@ -1,6 +1,23 @@
-﻿namespace Project_API.ValueObjects
+﻿using Project_API.Common;
+using System.Diagnostics.Metrics;
+
+namespace Project_API.ValueObjects
 {
-    public class UserCredentials
+    public class UserCredentials : ValueObject
     {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public UserCredentials() { }
+        public UserCredentials(string firstName, string lastName)
+        {
+            firstName = FirstName;
+            lastName = LastName;
+        }
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return FirstName;
+            yield return LastName;
+            
+        }
     }
 }
