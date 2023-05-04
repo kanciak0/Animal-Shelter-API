619807f3c834652b827a1c10db131f1c687a871a
Artur Madejski <artur.madejski.kanciak@wp.pl>
2023-05-04 17:17:21 +0200

    feat: Added Newtonsoft json library and created Stronglytypedidjsonconverter

--- Project_API/Domain/StronglyTypedIdJsonConverter.cs
+++ Project_API/Domain/StronglyTypedIdJsonConverter.cs
@@ -1,26 +1,18 @@
-﻿using System.Text.Json.Serialization;
-using System.Text.Json;
+﻿using Newtonsoft.Json;
 
 namespace Project_API.Domain
 {
     public class StronglyTypedIdJsonConverter<T> : JsonConverter<StronglyTypedId<T>> where T : class
     {
-        public override StronglyTypedId<T> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        public override StronglyTypedId<T>? ReadJson(JsonReader reader, Type objectType, StronglyTypedId<T>? existingValue, bool hasExistingValue, JsonSerializer serializer)
         {
-            string idString = reader.GetString();
-
-            Guid idGuid;
-            if (!Guid.TryParse(idString, out idGuid))
-            {
-                throw new JsonException($"Invalid Guid value '{idString}' in JSON.");
-            }
-            return new StronglyTypedId<T>(idGuid);
+            var guid = serializer.Deserialize<Guid>(reader);
+            return new StronglyTypedId<T>(guid);
         }
 
-
-        public override void Write(Utf8JsonWriter writer, StronglyTypedId<T> value, JsonSerializerOptions options)
+        public override void WriteJson(JsonWriter writer, StronglyTypedId<T>? value, JsonSerializer serializer)
         {
-            writer.WriteStringValue(value.ToString());
+            serializer.Serialize(writer, value);
         }
     }
 }
